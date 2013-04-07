e5eb033a7a103c3b523a11fbef23c09e1ff63305
Max <mczonk@gmail.com>
2013-04-07 18:51:44 +0200

    saved one byte by using int** instead of char**

--- mini7digit.c
+++ mini7digit.c
@@ -1,3 +1,3 @@
 #define L(u,v)c=a[1];while(*c)printf("%c%c%c%c",y&u?124:32,y&v?95:32,(y=("|O6VYNnX~^")[*c++-48]+1)&u*2?124:32,c[1]?32:10);
-main(int y,char**a){char*L(0,1)L(8,2)L(32,4)}
+main(int y,int**a){char*L(0,1)L(8,2)L(32,4)}
 
