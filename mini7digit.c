f1891412aefa39cf84659b2df793898c7d2f1875
Max <mczonk@gmail.com>
2013-04-07 18:41:39 +0200

    Reduced main

--- mini7digit.c
+++ mini7digit.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
 #define L(u,v) for(char *c = a[1];*c;) printf("%c%c%c%c", y&u?'|':32, y&v?'_':32, (y = ("|O6VYNnX~^")[*c++-48]+1)&u*2?'|':32, c[1]?' ':'\n');
-int main(int y, char **a) {
-	L(0,1);L(8,2);L(32,4);
-	return 0;
-}
+main(int y,char**a){L(0,1)L(8,2)L(32,4)}
 
