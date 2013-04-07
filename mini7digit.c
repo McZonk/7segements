a1d21da97d911a1696e8ff69e77a48123d257a70
Max <mczonk@gmail.com>
2013-04-07 18:40:35 +0200

    Shrink code

--- mini7digit.c
+++ mini7digit.c
@@ -1,25 +1,7 @@
-/*
- 1
-4 5
- 2
-6 7
- 3
- */
-
 #include <stdio.h>
-
-#define L(u,v) \
-for(char *c = a[1];*c;) printf("%c%c%c%c", \
-	   y & u ?   '|' : 32, \
-	   y & v ?   '_' : 32, \
-	   (y = ("|O6VYNnX~^")[*c++-48]+1) & u*2 ? '|' : 32, \
-	   c[1] ?    ' ' : '\n');
-
+#define L(u,v) for(char *c = a[1];*c;) printf("%c%c%c%c", y&u?'|':32, y&v?'_':32, (y = ("|O6VYNnX~^")[*c++-48]+1)&u*2?'|':32, c[1]?' ':'\n');
 int main(int y, char **a) {
-	L(0, 1);
-	L(8, 2);
-	L(32, 4);
-
+	L(0,1);L(8,2);L(32,4);
 	return 0;
 }
 
