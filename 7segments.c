5c98c311071d9e92e56bd004026e796f3ff84303
Max <mczonk@gmail.com>
2013-04-07 22:50:18 +0200

    Use [] instead of | for segments

--- 7segments.c
+++ 7segments.c
@@ -1,2 +1,2 @@
-#define L(u)c=a[1];for(;*c;)printf("%c%c%c%c",y&u/4?124:32,y&u?95:32,(y=a[0][*c++-46]+3)&u/2?124:32,c[1]?32:10);
+#define L(u)c=a[1];for(;*c;)printf("%c%c%c%c",y&u/4?91:32,y&u?95:32,(y=a[0][*c++-46]+3)&u/2?93:32,c[1]?32:10);
 main(int y,char**a){char*L(1)L(8)L(64)}
