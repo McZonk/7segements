7c8b3253ca5741fdd36e50de2154eb384a0c3a2a
Max <mczonk@gmail.com>
2013-04-07 22:27:13 +0200

    Changed table offset

--- 7segments.c
+++ 7segments.c
@@ -1,2 +1,2 @@
-#define L(u)c=a[1];for(;*c;)printf("%c%c%c%c",y&u/4?124:32,y&u?95:32,(y="v#\\l-jz$~n"[*c++-48]+1)&u/2?124:32,c[1]?32:10);
+#define L(u)c=a[1];for(;*c;)printf("%c%c%c%c",y&u/4?124:32,y&u?95:32,(y="t!Zj+hx$|l"[*c++-48]+3)&u/2?124:32,c[1]?32:10);
 main(int y,int**a){char*L(1)L(8)L(64)}
