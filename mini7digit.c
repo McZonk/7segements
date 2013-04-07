b5703fa20995fc5dd01f9026f6259b2ceeadcdb1
Max <mczonk@gmail.com>
2013-04-07 17:42:33 +0200

    Use a single call only (warning x86 only) Move the y assignment to another point in code

--- mini7digit.c
+++ mini7digit.c
@@ -8,23 +8,19 @@
 
 #include <stdio.h>
 
-void line(char** a, int u, int v) {
-	char *c = a[1];
-	while(*c) {
-		int y = ("|O6VYNnX~^")[*c++-48]+1;
-		
-		printf("%c%c%c%c",
-			   y & u ?   '|' : 32,
-			   y & v ?   '_' : 32,
-			   y & u*2 ? '|' : 32,
-			   c[0] ?    ' ' : '\n');
-	}
-}
+#define L(u,v) \
+for(char *c = a[1];*c;) { \
+printf("%c%c%c%c", \
+	   y & u ?   '|' : 32, \
+	   y & v ?   '_' : 32, \
+	   (y = ("|O6VYNnX~^")[*c++-48]+1) & u*2 ? '|' : 32, \
+	   c[1] ?    ' ' : '\n'); \
+};
 
 int main(int y, char **a) {
-	line(a, 0, 1);
-	line(a, 8, 2);
-	line(a, 32, 4);
+	L(0, 1);
+	L(8, 2);
+	L(32, 4);
 
 	return 0;
 }
