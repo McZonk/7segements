747bfb91cc1ca090733873caa9241dd055e81215
Max <mczonk@gmail.com>
2013-04-07 18:46:14 +0200

    gcc only no g++

--- mini7digit.c
+++ mini7digit.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
-#define L(u,v) for(char *c = a[1];*c;) printf("%c%c%c%c", y&u?'|':32, y&v?'_':32, (y = ("|O6VYNnX~^")[*c++-48]+1)&u*2?'|':32, c[1]?' ':'\n');
-main(int y,char**a){L(0,1)L(8,2)L(32,4)}
+#define L(u,v)c=a[1];while(*c)printf("%c%c%c%c",y&u?'|':32,y&v?'_':32,(y=("|O6VYNnX~^")[*c++-48]+1)&u*2?'|':32,c[1]?32:10);
+main(int y,char**a){char*L(0,1)L(8,2)L(32,4)}
 
