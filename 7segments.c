423e9aa3538cf8c65cf693e7a36403cba8596f6f
Max <mczonk@gmail.com>
2013-10-02 15:00:45 +0200

    Optimized code, still can remove \xff with a single character

--- 7segments.c
+++ 7segments.c
@@ -1,2 +1,2 @@
-#define L(u)c=a[1];for(;*c;)printf("%c%c%c%c",y&u/4?124:32,y&u?95:32,(y="w$]m.k{'ÿo"[*c++-48])&u/2?124:32,c[1]?32:10);
-main(int y,int**a){char*L(1)L(8)L(64)}
+#define L for(char y,*c=a[1];*c;)printf("%c%c%c%c",(y="w$]m.k{\xffo"[*c++-48])&u/4?124:32,y&u?95:32,y&u/2?124:32,*c?32:10);u*=8
+main(int u,char**a){u=1;L;L;L;}
