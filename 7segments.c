4ce479207fd6bef3761b4bf401adc2ea4c00aa4c
Max <mczonk@gmail.com>
2013-10-02 15:08:37 +0200

    Switched variable order to save 1 byte

--- 7segments.c
+++ 7segments.c
@@ -1,2 +1,2 @@
-#define L for(char y,*c=a[1];*c;)printf("%c%c%c%c",(y="w$]m.k{\xffo"[*c++-48])&u/4?124:32,y&u?95:32,y&u/2?124:32,*c?32:10);u*=8
-main(int u,char**a){u=1;L;L;L;}
+#define L for(char*c=a[1],y;*c;)printf("%c%c%c%c",(y="w$]m.k{\xffo"[*c++-48])&u/4?124:32,y&u?95:32,y&u/2?124:32,*c?32:10);u*=8;
+main(int u,char**a){u=1;L;L;L}
