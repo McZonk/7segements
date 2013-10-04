e4dfaad868cf5f29058a081422cdb4420d853311
Max <mczonk@gmail.com>
2013-10-04 18:25:38 +0200

    Use ! instead of |, looks nicer also saves two bytes

--- 7segments.c
+++ 7segments.c
@@ -1,2 +1,2 @@
-#define L for(char*c=a[1],y;*c;)printf("%c%c%c%c",(y="w$]m.k{%\177o"[*c++-48])&u/4?124:32,y&u?95:32,y&u/2?124:32,*c?32:10);u*=8;
+#define L for(char*c=a[1],y;*c;)printf("%c%c%c%c",(y="w$]m.k{%\177o"[*c++-48])&u/4?33:32,y&u?95:32,y&u/2?33:32,*c?32:10);u*=8;
 main(int u,char**a){u=1;L;L;L}
