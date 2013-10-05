fcae2a113402e4430cfd8d583ccc5921b2772643
Max <mczonk@gmail.com>
2013-10-05 16:30:26 +0200

    Improved table without escaped characters (-1 byte)

--- 7segments.c
+++ 7segments.c
@@ -1,2 +1,2 @@
-#define L for(char*c=a[1],y;*c;)printf("%c%c%c%c",(y="îHºÚ\\ÖöJşŞ"[*c++-48])&u/4?33:32,y&u?95:32,y&u/2?33:32,*c?32:10);u*=8;
+#define L for(char*c=a[1],y;*c;)printf("%c%c%c%c",(y="î$Ö¶<ºú&ş¾"[*c++-48])&u/2?33:32,y&u?95:32,y&u/4?33:32,*c?32:10);u*=8;
 main(int u,char**a){L;L;L}
