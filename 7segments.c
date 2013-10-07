ecefcf452f3941ca117f52da61f8a8cc79a1b573
Max <mczonk@gmail.com>
2013-10-07 08:19:34 +0200

    Use printf without puts again to save one more byte

--- 7segments.c
+++ 7segments.c
@@ -1,1 +1,1 @@
-main(int u,char**a){for(;u<129;u*=8){for(char*c=a[1],y;*c;)printf("%c%c%c ",(y="î$Ö¶<ºú&þ¾"[*c++-48])&u/2?33:32,y&u?95:32,y&u/4?33:32);puts("");}}
+main(int u,char**a){for(;u<129;u*=8)for(char*c=a[1],y;*c;)printf("%c%c%c%c",(y="î$Ö¶<ºú&þ¾"[*c++-48])&u/2?33:32,y&u?95:32,y&u/4?33:32,*c?32:10);}
