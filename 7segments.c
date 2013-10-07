7fc441ff876199bae81d250f10f931138f9be1bf
Max <mczonk@gmail.com>
2013-10-07 08:43:09 +0200

    Save one more byte by casting loop counter u to a char

--- 7segments.c
+++ 7segments.c
@@ -1,1 +1,1 @@
-main(int u,char**a){for(;u<129;u*=8)for(char*c=a[1],y;*c;)printf("%c%c%c%c",(y="î$Ö¶<ºú&þ¾"[*c++-48])&u/2?33:32,y&u?95:32,y&u/4?33:32,*c?32:10);}
+main(int u,char**a){for(char*c,y;y=u;u*=8)for(c=a[1];*c;)printf("%c%c%c%c",(y="î$Ö¶<ºú&þ¾"[*c++-48])&u/2?33:32,y&u?95:32,y&u/4?33:32,*c?32:10);}
